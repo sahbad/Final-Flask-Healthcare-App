ad3e53f47735b477fc9c518f6d880dc953970040
sahbad <saheedbadru@gmail.com>
2025-11-21 01:52:06 +0100

    chore: apply stashed changes  add misc, output, sample-config and update files

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def success():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
---
+++ sample-config.py
@@ -0,0 +1,2 @@
+MONGO_URI = "mongodb+srv://<username>:<password>@cluster0.xxxxx.mongodb.net/?retryWrites=true&w=majority"
+#replace <username> and <password> with actual mongoDB credentials
